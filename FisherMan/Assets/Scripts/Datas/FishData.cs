e47d2e812a4bb910b7446bb1ce5b303589d7b594
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-24 15:08:02 +0900

    [update]fishdataクラスをスクリプタブルオブジェクト化

--- FisherMan/Assets/Scripts/Datas/FishData.cs
+++ FisherMan/Assets/Scripts/Datas/FishData.cs
@@ -1,16 +1,27 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-public class FishData : MonoBehaviour
+[CreateAssetMenu(fileName = "FishData", menuName = "EntityDatas/FishData")]
+public class FishData : ScriptableObject
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    /// <summary>魚の名前</summary>
+    [SerializeField, Header("魚の名前")]
+    private string _fishName;
+    /// <summary>魚のHP</summary>
+    [SerializeField, Header("魚のHP")]
+    private int _fishHp;
+    /// <summary>魚の売値</summary>
+    [SerializeField, Header("魚の売値")]
+    private int _fishPrice;
+    /// <summary>魚の魚影のサイズ</summary>
+    [SerializeField, Header("魚の魚影のサイズ")]
+    private int _fishShadowSize;
+    /// <summary>問題の難易度</summary>
+    [SerializeField, Header("問題の難易度")]
+    private int _fishLevel;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string FishName => _fishName;
+    public int FishHp => _fishHp;
+    public int FishPrice => _fishPrice;
+    public int FishShadowSize => _fishShadowSize;
+    public int FishLevel => _fishLevel;
 }
