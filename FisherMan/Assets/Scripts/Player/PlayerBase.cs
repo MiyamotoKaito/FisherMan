01ec97702d3d4f8654225cebf415583bdf4d6e19
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-25 11:33:06 +0900

    [add]インプットシステムとステートenumを管理するプレイヤーの基底クラス

---
+++ FisherMan/Assets/Scripts/Player/PlayerBase.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerBase : MonoBehaviour
+{
+    protected InputBuffer _inputBuffer;
+    /// <summary>現在のプレイヤーの状態
+    /// 初期はIdle
+    /// </summary>
+    protected PlayerStates _currentState = PlayerStates.Idle;
+    public PlayerStates CurrentState => _currentState;
+    /// <summary>
+    /// 入力アクションを呼ぶ
+    /// </summary>
+    protected void BaseAwake()
+    {
+        _inputBuffer = new InputBuffer();
+        _inputBuffer.Enable();
+    }
+    /// <summary>
+    /// 入力を無効化する
+    /// </summary>
+    protected void BaseDisable()
+    {
+        _inputBuffer.Disable();
+    }
+}
+public enum PlayerStates
+{
+    Idle,
+    Move,
+    Fishing,
+    Typing
+}
