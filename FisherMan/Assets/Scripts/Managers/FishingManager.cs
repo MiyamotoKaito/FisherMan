dc62f30c120f9b6f02831841c4b4b644671868e5
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-25 19:08:04 +0900

    [add]魚が餌にかかった時のイベント

--- FisherMan/Assets/Scripts/Managers/FishingManager.cs
+++ FisherMan/Assets/Scripts/Managers/FishingManager.cs
@@ -1,8 +1,11 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class FishingManager : MonoBehaviour
 {
     public static FishingManager Instance;
+    /// <summary>魚が掛かった時のイベント</summary>
+    public event Action<FishData> OnFallBait;
     private void Awake()
     {
         if (!Instance)
@@ -14,5 +17,13 @@ public class FishingManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+    }
+    /// <summary>
+    /// 魚が餌に掛かった時に呼び出す
+    /// </summary>
+    /// <param name="fishData"></param>
+    private void FallBait(FishData fishData)
+    {
+        OnFallBait.Invoke(fishData);
     }
 }
