8424a1460f98f2ea85b1e986610a1ed36392c3ab
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-25 12:10:35 +0900

    [add]ゲーム進行を管理する

---
+++ FisherMan/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+public enum PlayerStates
+{
+    Idle,
+    Move,
+    Fishing,
+    Typing
+}
