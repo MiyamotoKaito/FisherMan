d4032cdd87d1f13a72bb5290bba347b8eba0f1df
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-25 13:46:37 +0900

    [add]プレイヤーの動きを管理する派生クラス

---
+++ FisherMan/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerMove : PlayerBase
+{
+    [SerializeField, Header("プレイヤーの歩くスピード")]
+    private float _speed;
+
+    private Rigidbody _rb;
+
+    private void Awake()
+    {
+        base.BaseAwake();
+    }
+}
