94b320c3b38335f5ce27e3b92cd72ad5693c67e7
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-25 14:07:54 +0900

    [add]アニメーションをステートによって管理するオーバーライド関数

--- FisherMan/Assets/Scripts/Player/PlayerMove.cs
+++ FisherMan/Assets/Scripts/Player/PlayerMove.cs
@@ -7,8 +7,31 @@ public class PlayerMove : PlayerBase
 
     private Rigidbody _rb;
 
+
     private void Awake()
     {
         base.BaseAwake();
+    }
+    /// <summary>
+    /// アニメーションを管理する
+    /// </summary>
+    /// <param name="state"></param>
+    protected override void OnStateEnter(PlayerStates state)
+    {
+        switch (state)
+        {
+            case PlayerStates.Idle:
+                //ここにアニメーションのフラグ
+                break;
+            case PlayerStates.Move:
+                //ここにアニメーションのフラグ
+                break;
+            case PlayerStates.Fishing:
+                //ここにアニメーションのフラグ
+                break;
+            case PlayerStates.Typing:
+                //ここにアニメーションのフラグ
+                break;
+        }
     }
 }
