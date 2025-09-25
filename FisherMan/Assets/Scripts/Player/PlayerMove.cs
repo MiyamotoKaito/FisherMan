10753a9f8c74925083a905749882f9206558f064
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-25 14:08:49 +0900

    [add]プレイヤーのい動きを管理する関数

--- FisherMan/Assets/Scripts/Player/PlayerMove.cs
+++ FisherMan/Assets/Scripts/Player/PlayerMove.cs
@@ -1,16 +1,52 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : PlayerBase
 {
     [SerializeField, Header("プレイヤーの歩くスピード")]
     private float _speed;
-
+    private Vector2 _currentMove;
     private Rigidbody _rb;
 
 
     private void Awake()
     {
         base.BaseAwake();
+        _rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+
+    }
+    private void OnEnable()
+    {
+        _inputBuffer.Player.Move.performed += OnInputWalk;
+    }
+    private void OnDisable()
+    {
+        base.BaseDisable();
+    }
+    /// <summary>
+    /// 移動を管理する
+    /// </summary>
+    /// <param name="context"></param>
+    private void OnInputWalk(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _currentMove = context.ReadValue<Vector2>();
+        }
+        else if (context.canceled)
+        {
+            _currentMove = Vector2.zero;
+        }
+    }
+    /// <summary>
+    /// 実際にプレイヤーを動かす関数
+    /// </summary>
+    private void PlayerWalk()
+    {
+
     }
     /// <summary>
     /// アニメーションを管理する
@@ -23,7 +59,7 @@ public class PlayerMove : PlayerBase
             case PlayerStates.Idle:
                 //ここにアニメーションのフラグ
                 break;
-            case PlayerStates.Move:
+            case PlayerStates.Walk:
                 //ここにアニメーションのフラグ
                 break;
             case PlayerStates.Fishing:
