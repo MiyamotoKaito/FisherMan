300d6e06815c3b08a6b3d419247b0a20afab37c1
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-26 07:33:27 +0900

    [add]魚が餌にかかったらデータを代入する

--- FisherMan/Assets/Scripts/Fish/FishBase.cs
+++ FisherMan/Assets/Scripts/Fish/FishBase.cs
@@ -2,5 +2,24 @@
 
 public class FishBase : MonoBehaviour
 {
-    
+    private string _name;
+    private int _hp;
+    private int _price;
+    private int _shadowSize;
+    private int _level;
+    private int _timer;
+
+    /// <summary>
+    /// 釣り竿を投げ入れたら魚に値を設定する
+    /// </summary>
+    /// <param name="fishData"></param>
+    private void SetParameter(FishData fishData)
+    {
+        _name = fishData.name;
+        _hp = fishData.FishHp;
+        _price = fishData.FishPrice;
+        _shadowSize = fishData.FishShadowSize;
+        _level = fishData.FishLevel;
+        _timer = fishData.FishTimer;
+    }
 }
