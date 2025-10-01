d83acd27b74dec6d06ddb582725cca369c2200fd
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-10-01 16:24:00 +0900

    [update]データからベースに置き換え

--- FisherMan/Assets/Scripts/Managers/FishingManager.cs
+++ FisherMan/Assets/Scripts/Managers/FishingManager.cs
@@ -5,7 +5,7 @@ public class FishingManager : MonoBehaviour
 {
     public static FishingManager Instance;
     /// <summary>魚が掛かった時のイベント</summary>
-    public event Action<FishData> OnFallBait;
+    public event Action<FishBase> OnFallBait;
     private void Awake()
     {
         if (!Instance)
@@ -21,9 +21,9 @@ public class FishingManager : MonoBehaviour
     /// <summary>
     /// 魚が餌に掛かった時に呼び出す
     /// </summary>
-    /// <param name="fishData"></param>
-    private void FallBait(FishData fishData)
+    /// <param name="fishBase"></param>
+    private void FallBait(FishBase fishBase)
     {
-        OnFallBait.Invoke(fishData);
+        OnFallBait.Invoke(fishBase);
     }
 }
